49430c592d27af9729b65627e958b0b35325d1ad
anoziekelechi <68008223+anoziekelechi@users.noreply.github.com>
2026-08-09 08:50:06 +0000

    Update tyoes.ts

--- tyoes.ts
+++ tyoes.ts
@@ -1,4 +1,30 @@
+// src/types/user/profile.ts
+export interface User {
+  id: number;
+  email: string;
+  surname: string;
+  othernames: string;
+  phone?: string | null;
+  verified: boolean;
+  disabled: boolean;
+  date_added: string;
+}
 
+
+
+
+// src/types/user/index.ts
+export * from "./profile";
+export * from "./registration";
+export * from "./login";
+export * from "./common";
+
+
+
+// src/types/index.ts
+export * from "./user";
+export * from "./product";
+export * from "./common";
 
 
 
