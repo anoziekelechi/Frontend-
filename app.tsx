d0bc5cd1d05dbc13f88d4586294c0f163a315781
anoziekelechi <68008223+anoziekelechi@users.noreply.github.com>
2026-08-09 15:31:03 +0000

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,4 +1,21 @@
+//main.tsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+//import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import {router} from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap-icons/font/bootstrap-icons.css';
+//<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
+
+
+//app.tsx
 import { createBrowserRouter, Outlet } from 'react-router-dom';
 import Navigation from './base/Navigation';
 import Home from './pages/Home'
